8fe829a84436ae5d7135cc73234f852da68412ba
weichichou <weichi.cc@gmail.com>
2019-10-26 00:05:45 +0200

    finish step 4

--- src/component/Quote.js
+++ src/component/Quote.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-
+import './Quote.css'
 
 export default class Quote extends Component {
+    state = {
+        like: null
+    }
+
+    like = () => {
+        this.setState({like: true})
+    }
+
+    dislike = () => {
+        this.setState({like: false})
+    }
+
     render(){
+       let className;
+       if (this.state.like === true) {
+          className = 'isLiked'
+       } else if (this.state.like === false) {
+           className = 'isDisliked'
+       } else {className = ''};
+
         return (
             <div>
-                <p>{this.props.quoteText}</p>
-                <p>By: {this.props.quoteAuthor}</p>
+                <p className={className}>{this.props.quoteText}</p>
+                <p> By: {this.props.quoteAuthor} {''}
+                    <button onClick={this.dislike}>:(</button> {''}
+                    <button onClick={this.like}>:)</button>
+                </p>
                 &nbsp;
             </div>
         )
